760b1816c4fd5b9bfe26327e8b8defee6fc40e05
JP2620 <juan.saucedo@mi.unc.edu.ar>
2023-05-25 02:19:16 -0300

    Added more tests

--- src/test/java/com/paygoal/api/ProductControllerTest.java
+++ src/test/java/com/paygoal/api/ProductControllerTest.java
@@ -16,12 +16,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -126,5 +130,95 @@ class ProductControllerTest {
 	public void testDeleteProduct() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/products/{id}", 1L))
 				.andExpect(MockMvcResultMatchers.status().isOk());
+	}
+
+	@Test
+	public void testGetAllProducts() throws Exception {
+		Product product1 = new Product();
+		product1.setId(1L);
+		product1.setName("Test Product 1");
+		product1.setPrice(100L);
+		product1.setQuantity(10L);
+		product1.setDescription("Test Description 1");
+
+		Product product2 = new Product();
+		product2.setId(2L);
+		product2.setName("Test Product 2");
+		product2.setPrice(200L);
+		product2.setQuantity(20L);
+		product2.setDescription("Test Description 2");
+
+		Mockito.when(productService.getAllProducts()).thenReturn(List.of(ProductDto.of(product1), ProductDto.of(product2)));
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/products"))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$[0].id", Matchers.is(1)))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[0].name", Matchers.is("Test Product 1")))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[0].price", Matchers.is(100)))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[0].quantity", Matchers.is(10)))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[0].description", Matchers.is("Test Description 1")))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[1].id", Matchers.is(2)))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[1].name", Matchers.is("Test Product 2")))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[1].price", Matchers.is(200)))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[1].quantity", Matchers.is(20)))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[1].description", Matchers.is("Test Description 2")));
+	}
+
+	@Test
+	public void testGetProductByName() throws Exception {
+		Product product = new Product();
+		product.setId(1L);
+		product.setName("Test Product");
+		product.setPrice(100L);
+		product.setQuantity(10L);
+		product.setDescription("Test Description");
+
+		Mockito.when(productService.getByName(Mockito.anyString())).thenReturn(ProductDto.of(product));
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/products/name/{name}", "Test Product"))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.id", Matchers.is(1)))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.name", Matchers.is("Test Product")))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.price", Matchers.is(100)))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.quantity", Matchers.is(10)))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.description", Matchers.is("Test Description")));
+	}
+
+	@Test
+	public void testGetProductPage() throws Exception {
+		Product product1 = new Product();
+		product1.setId(1L);
+		product1.setName("Test Product 1");
+		product1.setPrice(100L);
+		product1.setQuantity(10L);
+		product1.setDescription("Test Description 1");
+
+		Product product2 = new Product();
+		product2.setId(2L);
+		product2.setName("Test Product 2");
+		product2.setPrice(200L);
+		product2.setQuantity(20L);
+		product2.setDescription("Test Description 2");
+
+		Page<ProductDto> productPage = new PageImpl<>(List.of(ProductDto.of(product1), ProductDto.of(product2)));
+
+		Mockito.when(productService.getPage(Mockito.any(Pageable.class))).thenReturn(productPage);
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/products/page")
+				.param("pageNo", "0")
+				.param("pageSize", "10")
+				.param("sortBy", "id")
+				.param("sortDirection", "ASC"))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.content[0].id", Matchers.is(1)))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.content[0].name", Matchers.is("Test Product 1")))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.content[0].price", Matchers.is(100)))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.content[0].quantity", Matchers.is(10)))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.content[0].description", Matchers.is("Test Description 1")))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.content[1].id", Matchers.is(2)))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.content[1].name", Matchers.is("Test Product 2")))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.content[1].price", Matchers.is(200)))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.content[1].quantity", Matchers.is(20)))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.content[1].description", Matchers.is("Test Description 2")));
 	}
 }
